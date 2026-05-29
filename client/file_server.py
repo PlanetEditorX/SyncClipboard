4e4684ebb6fd6f39bac1ad367890fdf9d6aab1ce
PlanetEditorX <962129122@qq.com>
2026-05-30 00:22:42 +0800

    修改更新文件列表

--- client/file_server.py
+++ client/file_server.py
@@ -99,15 +99,13 @@ class FileServer:
             else:
                 logger.info(f"更新文件列表 - 请求来自: {client_ip}")
                 latest = data.get("latest_global")
-                latest_file_id = latest["file_id"]
                 # 获取本地已存储的文件
                 file_latest = get_current_file()
-                if any(d.get("file_id") == latest_file_id for d in file_latest):
-                    logger.info(f"文件ID未变化 ({latest_file_id})，跳过写入")
+                if latest == file_latest:
+                    logger.info(f"文件未变化，跳过写入")
                 else:
                     # 只有当 file_id 不一致时才写入
-                    file_latest = []
-                    file_latest.append(latest)
+                    file_latest = latest
                     with open(FILES_LATEST_FILE, "w", encoding="utf-8") as f:
                         json.dump(file_latest, f, ensure_ascii=False, indent=2)
                     logger.info("file_latest文件已更新")
